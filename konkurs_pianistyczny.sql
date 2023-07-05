61e023685bb28998a7f7b28f2f5f15927571aa3a
Jałokim <robimy_po_swojemu@o2.pl>
2023-07-05 23:04:46 +0200

    sql database

--- konkurs_pianistyczny.sql
+++ konkurs_pianistyczny.sql
@@ -1,217 +1,198 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Czas generowania: 16 Cze 2023, 08:56
--- Wersja serwera: 10.4.17-MariaDB
--- Wersja PHP: 8.0.0
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE kompozytorzy (
+  Id SERIAL PRIMARY KEY,
+  Imie varchar(50) DEFAULT NULL,
+  Nazwisko varchar(50) DEFAULT NULL
+);
+
+INSERT INTO kompozytorzy (Imie, Nazwisko) VALUES
+('Fryderyk', 'Chopin'),
+('Piotr', 'Czajkowski'),
+('Antonin', 'Dvorak'),
+('Jacek', 'Kaczmarski');
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE pianisci (
+  Id SERIAL PRIMARY KEY,
+  Imie varchar(50) DEFAULT NULL,
+  Nazwisko varchar(50) DEFAULT NULL,
+  username varchar(50) NOT NULL,
+  password varchar(50) NOT NULL,
+  password_hash varchar(60) NOT NULL,
+  email varchar(50) NOT NULL
+);
 
---
--- Baza danych: `konkurs_pianistyczny`
---
 
--- --------------------------------------------------------
+INSERT INTO pianisci (Imie, Nazwisko, username, password, password_hash, email) VALUES
+('Adam', 'Małysz', 'Adam', 'Małamysz', '$2y$10$cfXJUmZjU9Xkv/pMzwcovuAdJucEJV4leMAcQbxnZmel60FLm/NV2', 'małamysz@gmail.com'),
+('Czesław', 'Małamyszyński', 'czeslaw', 'zaq', '$2y$10$sDQqhM2RKnrYlG3b3lnV0uvP/wvCYeBpovursMtvnsZ/zwkIOAFPW', 'czeslaw@gmail.com'),
+('Sułtan', 'Kosmitów', 'otyly_pan', 'zxcv', '$2y$10$ixvtmfYTeF53JCtOyZrm/OLEPeAX8kezD4T.rX.gQDdCmJLQkpVPS', 'otyly_pan@o2.pl'),
 
---
--- Struktura tabeli dla tabeli `kompozytorzy`
---
 
-CREATE TABLE `kompozytorzy` (
-  `Id` int(11) NOT NULL,
-  `Imie` varchar(50) DEFAULT NULL,
-  `Nazwisko` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE pracownicy (
+  Id SERIAL PRIMARY KEY,
+  Imie varchar(50) DEFAULT NULL,
+  Nazwisko varchar(50) DEFAULT NULL,
+  username varchar(50) NOT NULL,
+  password varchar(50) NOT NULL,
+  password_hash varchar(60) NOT NULL,
+  email varchar(50) NOT NULL
+);
 
---
--- Zrzut danych tabeli `kompozytorzy`
---
+INSERT INTO pracownicy (Imie, Nazwisko, username, password, password_hash, email) VALUES
+('Admin', 'Adminowski', 'Admin', 'zaq', '$2y$10$sDQqhM2RKnrYlG3b3lnV0uvP/wvCYeBpovursMtvnsZ/zwkIOAFPW', 'admin@gmail.com'),
+('Barbara', 'Barbara', 'Pani Basia', 'zaq', '$2y$10$sDQqhM2RKnrYlG3b3lnV0uvP/wvCYeBpovursMtvnsZ/zwkIOAFPW', 'pani_b@gmail.com');
 
-INSERT INTO `kompozytorzy` (`Id`, `Imie`, `Nazwisko`) VALUES
-(1, 'Fryderyk', 'Chopin'),
-(3, 'Piotr', 'Czajkowski'),
-(4, 'Antonin', 'Dvorak'),
-(5, 'Jacek', 'Kaczmarski');
 
--- --------------------------------------------------------
+CREATE TABLE utwory (
+  Id SERIAL PRIMARY KEY,
+  Tytul varchar(50) NOT NULL,
+  Kompozytor_Id INTEGER NOT NULL,
+  FOREIGN KEY (Kompozytor_Id) REFERENCES kompozytorzy(id)
+);
 
---
--- Struktura tabeli dla tabeli `pianisci`
---
+INSERT INTO utwory (Tytul, Kompozytor_Id) VALUES
+('Obława I', 4),
+('Epitafium dla Włodzimierza Wysockiego', 4),
+('Allegro con Fuoco', 3),
+('Etiuda Rewolucyjna', 1),
+('Jezioro Łabędzie', 2);
 
-CREATE TABLE `pianisci` (
-  `Id` int(11) NOT NULL,
-  `Imie` varchar(50) DEFAULT NULL,
-  `Nazwisko` varchar(50) DEFAULT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `password_hash` varchar(60) NOT NULL,
-  `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE wykonania (
+  Id SERIAL PRIMARY KEY,
+  Ocena NUMERIC(2, 1) DEFAULT NULL,
+  Zaakceptowany BOOLEAN DEFAULT FALSE,
+  Wykonany BOOLEAN DEFAULT FALSE,
+  Harmonogram INTEGER DEFAULT NULL,
+  Data_zgloszenia TIMESTAMP NOT NULL DEFAULT NOW(),
+  Pianisci_Id INTEGER NOT NULL,
+  Utwory_Id INTEGER NOT NULL,
+  FOREIGN KEY (Pianisci_Id) REFERENCES pianisci(id),
+  FOREIGN KEY (Utwory_Id) REFERENCES utwory(id)
+);
 
---
--- Zrzut danych tabeli `pianisci`
---
+ALTER TABLE wykonania
+ADD CONSTRAINT Ocena CHECK (Ocena >= 0 AND ocena <= 6);
 
-INSERT INTO `pianisci` (`Id`, `Imie`, `Nazwisko`, `username`, `password`, `password_hash`, `email`) VALUES
-(2, 'Adam', 'Małysz', 'Adam', 'Małamysz', '$2y$10$cfXJUmZjU9Xkv/pMzwcovuAdJucEJV4leMAcQbxnZmel60FLm/NV2', 'małamysz@gmail.com'),
-(3, 'Czesław', 'Małamyszyński', 'czeslaw', 'zaq', '$2y$10$sDQqhM2RKnrYlG3b3lnV0uvP/wvCYeBpovursMtvnsZ/zwkIOAFPW', 'czeslaw@gmail.com'),
-(4, 'Sułtan', 'Kosmitów', 'otyly_pan', 'zxcv', '$2y$10$ixvtmfYTeF53JCtOyZrm/OLEPeAX8kezD4T.rX.gQDdCmJLQkpVPS', 'otyly_pan@o2.pl'),
-(7, NULL, NULL, 'aaa', 'aaa', '$2y$10$xx/A32RBaZeeEh4B.QUTs.wMxc9.iWz.KlStMhADN8OwBShiYCRBK', 'aaa@gmail.com'),
-(8, NULL, NULL, 'ccc', 'ccc', '$2y$10$.WUwotXv9LdDZcksrGAd9eW1SgcWyU3h3njhgoCUbh8osfs9XZz5u', 'ccc@gmail.com'),
-(9, NULL, NULL, 'dsdas', 'sdadas', '$2y$10$Uyj99EFcQJsxbQpNfHsqe.bXCTDHk36a0uAc5isun1vVSCTidEfBy', 'dsadas@o2.pl'),
-(10, NULL, NULL, 'dasdas', 'dasdasdas', '$2y$10$VTS1bxNIP5qdVsSPVzLgQefwCOPRSFrozsqCJZK4zX3KOhKeXKvd.', 'dasdas@o2.pl'),
-(11, NULL, NULL, 'saas', 'asasas', '$2y$10$lbS9j4asyYOQK411y1gOA.Y5OMR/kxpwlh.VR.2tdT6wc2Y66dQim', 'asas@o2.pl'),
-(12, NULL, NULL, 'dew', 'dasdasdas', '$2y$10$47XyWIbfpACXwbxsWVkfauy0CqGdxVcWr8rO54I3NFB66imdVOWfW', 'dasas@o2.pl');
+CREATE TABLE etapy_konkursu (
+  Id INTEGER PRIMARY KEY,
+  nazwa varchar(50) NOT NULL
+);
 
--- --------------------------------------------------------
+INSERT INTO etapy_konkursu (Id, nazwa) VALUES 
+    (1, 'Stage 1: registration of submissions'),
+    (2, 'Stage 2: verification of submissions'),
+    (3, 'Stage 3: performed and evaluation of compositions'),
+    (4, 'Stage 4: competition concluded');
 
---
--- Struktura tabeli dla tabeli `pracownicy`
---
+CREATE TABLE konkurs (
+  Id SERIAL PRIMARY KEY,
+  Data_zakonczenia TIMESTAMP DEFAULT NULL,
+  Etap_id INTEGER NOT NULL
+  FOREIGN KEY (Etap_id) REFERENCES etapy_konkursu(id)
+);
 
-CREATE TABLE `pracownicy` (
-  `Id` int(11) NOT NULL,
-  `Imie` varchar(50) NOT NULL,
-  `Nazwisko` varchar(50) NOT NULL,
-  `Login` varchar(50) NOT NULL,
-  `Haslo` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO konkurs (Etap_id) VALUES
+(1);
 
--- --------------------------------------------------------
+-- sprawdzanie czy zgłoszenia są otwarte
+CREATE OR REPLACE FUNCTION aktualizuj_czy_zakonczony() RETURNS TRIGGER AS $$
+BEGIN
+  IF CURRENT_TIMESTAMP > (SELECT data_zakonczenia FROM konkurs LIMIT 1) THEN 
+    UPDATE konkurs SET etap_id = 2;
+	  RETURN NULL;
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
---
--- Struktura tabeli dla tabeli `utwory`
---
+CREATE TRIGGER sprawdz_data_zakonczenia
+BEFORE INSERT ON wykonania
+FOR EACH ROW
+EXECUTE FUNCTION aktualizuj_czy_zakonczony();
 
-CREATE TABLE `utwory` (
-  `Id` int(11) NOT NULL,
-  `Tytul` varchar(50) NOT NULL,
-  `Kompozytor_Id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- sprawdzenie czy nowo dodawany utwór należy do innego kompozytora niż dotychcza
+-- dozwolone utwory są filtrowane od strony klienta a od strony serwera dodatkowo sprawdzane
+CREATE OR REPLACE FUNCTION sprawdz_czy_rozni_kompozytorzy() RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.Utwory_Id IN (	SELECT u.id as u_id
+	FROM utwory as u JOIN kompozytorzy as k ON u.kompozytor_id=k.id 
+	WHERE k.id NOT IN (SELECT u.kompozytor_id FROM wykonania as w JOIN utwory as u ON w.utwory_id=u.id WHERE w.pianisci_id = NEW.pianisci_id)) THEN
+	RETURN NEW;
+  END IF;
+  RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
 
---
--- Zrzut danych tabeli `utwory`
---
+CREATE TRIGGER sprawdz_czy_rozni_kompozytorzy
+BEFORE INSERT ON wykonania
+FOR EACH ROW
+EXECUTE FUNCTION sprawdz_czy_rozni_kompozytorzy();
 
-INSERT INTO `utwory` (`Id`, `Tytul`, `Kompozytor_Id`) VALUES
-(1, 'Obława I', 5),
-(2, 'Epitafium dla Włodzimierza Wysockiego', 5),
-(5, 'Allegro con Fuoco', 4),
-(6, 'Etiuda Rewolucyjna', 1);
 
--- --------------------------------------------------------
+-- Funkcja do generownaia harmonogramu
+CREATE OR REPLACE FUNCTION ustaw_harmonogram() RETURNS VOID AS $$
+DECLARE
+	total_rows INT;
+	
+BEGIN
+	SELECT COUNT(*) INTO total_rows FROM wykonania WHERE zaakceptowany=true;
 
---
--- Struktura tabeli dla tabeli `wykonania`
---
+    UPDATE wykonania
+    SET harmonogram = CASE
+		WHEN (subquery.rn % 3) = 1 THEN (subquery.rn/3 + 1)
+		WHEN (subquery.rn % 3) = 2 THEN (subquery.rn/3 + 1) + total_rows/3
+		WHEN (subquery.rn % 3) = 0 THEN (subquery.rn/3) + 2*total_rows/3
+	END
+    FROM (
+        SELECT id, pianisci_id, ROW_NUMBER() OVER (ORDER BY pianisci_id) AS rn
+        FROM wykonania
+		WHERE zaakceptowany=true
+    ) AS subquery
+    WHERE wykonania.id = subquery.id;
+END;
+$$ LANGUAGE plpgsql;
 
-CREATE TABLE `wykonania` (
-  `Id` int(11) NOT NULL,
-  `Ocena` int(11) DEFAULT NULL,
-  `Pianisci_Id` int(11) NOT NULL,
-  `Utwory_Id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Trigger sprawdzający czy poprzedni utwór został już oceniony by móc ocenić kolejny
+CREATE OR REPLACE FUNCTION czy_poprzednie_wykonanie_ocenione()
+RETURNS TRIGGER AS $$
 
---
--- Indeksy dla zrzutów tabel
---
+BEGIN
+	IF (((SELECT etap_id FROM konkurs) = 3) AND NEW.harmonogram <> 1 AND (SELECT ocena FROM wykonania WHERE harmonogram = (NEW.harmonogram-1)) IS NULL) THEN
+		RETURN NULL;
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
---
--- Indeksy dla tabeli `kompozytorzy`
---
-ALTER TABLE `kompozytorzy`
-  ADD PRIMARY KEY (`Id`);
+CREATE TRIGGER czy_poprzednie_wykonanie_ocenione
+BEFORE UPDATE ON wykonania
+FOR EACH ROW
+EXECUTE FUNCTION czy_poprzednie_wykonanie_ocenione();
 
---
--- Indeksy dla tabeli `pianisci`
---
-ALTER TABLE `pianisci`
-  ADD PRIMARY KEY (`Id`);
 
---
--- Indeksy dla tabeli `pracownicy`
---
-ALTER TABLE `pracownicy`
-  ADD PRIMARY KEY (`Id`);
+-- Trigger sprawdzający czy wszystkie utowry zostały ocenione i automatycznie przełącza na 4. etap konkursu
+CREATE OR REPLACE FUNCTION czy_wszystkie_wykonania_ocenione()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (SELECT COUNT(*) FROM wykonania WHERE zaakceptowany = TRUE) = (SELECT COUNT(*) FROM wykonania WHERE ocena IS NOT NULL AND zaakceptowany = TRUE) THEN
+        UPDATE konkurs SET etap_id = 4;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
---
--- Indeksy dla tabeli `utwory`
---
-ALTER TABLE `utwory`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `Kompozytor_Id` (`Kompozytor_Id`);
+CREATE TRIGGER czy_wszystkie_wykonania_ocenione
+AFTER UPDATE ON wykonania
+FOR EACH ROW
+EXECUTE FUNCTION czy_wszystkie_wykonania_ocenione();
 
---
--- Indeksy dla tabeli `wykonania`
---
-ALTER TABLE `wykonania`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `Utwory_Id` (`Utwory_Id`),
-  ADD KEY `Pianisci_Id` (`Pianisci_Id`);
 
---
--- AUTO_INCREMENT dla zrzuconych tabel
---
-
---
--- AUTO_INCREMENT dla tabeli `kompozytorzy`
---
-ALTER TABLE `kompozytorzy`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT dla tabeli `pianisci`
---
-ALTER TABLE `pianisci`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT dla tabeli `pracownicy`
---
-ALTER TABLE `pracownicy`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `utwory`
---
-ALTER TABLE `utwory`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT dla tabeli `wykonania`
---
-ALTER TABLE `wykonania`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `utwory`
---
-ALTER TABLE `utwory`
-  ADD CONSTRAINT `utwory_ibfk_1` FOREIGN KEY (`Kompozytor_Id`) REFERENCES `kompozytorzy` (`Id`);
-
---
--- Ograniczenia dla tabeli `wykonania`
---
-ALTER TABLE `wykonania`
-  ADD CONSTRAINT `wykonania_ibfk_1` FOREIGN KEY (`Utwory_Id`) REFERENCES `utwory` (`Id`),
-  ADD CONSTRAINT `wykonania_ibfk_2` FOREIGN KEY (`Pianisci_Id`) REFERENCES `pianisci` (`Id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Widok do wyświetlania zbiorczej oceny pianistów
+CREATE VIEW ocena_pianistow AS
+	SELECT pianisci_id as id, p.imie as pianista_imie, p.nazwisko as pianista_nazwisko, SUM(w.ocena) as ocena_calkowita 
+  FROM wykonania as w 
+  JOIN pianisci as p ON w.pianisci_id = p.id 
+  WHERE zaakceptowany = TRUE AND ocena IS NOT NULL 
+  GROUP BY pianisci_id, p.imie, p.nazwisko 
+  ORDER BY ocena_calkowita DESC;
